fcb7e63326b038dd8ddfd47c1c31574efac83af0
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-05 18:58:19 +0100

    fixing products settings

--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var productsRouter = express.Router();
-const products = require('../public/data/products.json');
+const productsData = require('../public/data/products.json');
 
 productsRouter.get('/', function(req, res, next) {
-    res.json(products);
+    res.json(productsData);
     }
     );
 
-productsRouter.get('/:id', function(req, res, next) {
-    res.json(products[req.params.id]);
-    }
-    );
+    router.get('/:id', (req, res) => {
+        const productId = req.params.id;
+        const product = productsData.find((p) => p.id === productId);
+      
+        if (product) {
+          res.json({
+            product,
+          });
+        } else {
+          res.status(404).json({
+            message: 'Produit non trouvé',
+          });
+        }
+      });
 
+router.get('/:id/:qt', (req, res) => {
+  const productId = req.params.id;
+  const quantity = parseInt(req.params.qt, 10);
+  const product = productsData.find((p) => p.id === productId);
+
+  if (product) {
+    const totalPrice = quantity * product.price;
+    res.json({
+      totalPrice,
+    });
+  } else {
+    res.status(404).json({
+      message: 'Produit non trouvé',
+    });
+  }
+});
     
 module.exports = productsRouter;
